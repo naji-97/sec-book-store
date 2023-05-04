1b330615e98016842dd2a9f4fb8c32cd9be67404
Naji-97 <elbairabennaji@gmail.com>
2023-05-04 15:21:27 +0100

    setup my project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/books" element={<BookList />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
