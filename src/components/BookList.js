c33fc82b13f2dd5c6775b707700a3fb25178de16
Naji-97 <elbairabennaji@gmail.com>
2023-05-09 13:16:06 +0100

    create Button component and implement addBook and removeBook function

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,30 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 function BookList() {
-  const [books] = useState([
-    {
-      id: 1,
-      title: 'Setup development environment',
-      author: 'Naji',
-    },
-    {
-      id: 2,
-      title: 'Develop website and add content',
-      author: 'Ali',
-    },
-    {
-      id: 3,
-      title: 'Deploy to live server',
-      author: 'Imane',
-    },
-  ]);
+  const books = useSelector((state) => state.books.books);
 
   return (
     <>
       {books.map((book) => (
-        <Book key={book.id} id={book.id} title={book.title} author={book.author} />
+        <Book
+          key={book.item_id}
+          id={book.item_id}
+          title={book.title}
+          author={book.author}
+
+        />
       ))}
       <Form />
     </>
