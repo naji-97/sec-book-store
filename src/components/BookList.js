9f74f31c3834f5a54840860b5b3b4abc44d5136d
Naji-97 <elbairabennaji@gmail.com>
2023-05-10 16:31:46 +0100

    connect to Api and implement the add and remove functionlity

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 function BookList() {
+  const dispatch = useDispatch();
+
   const books = useSelector((state) => state.books.books);
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
     <>
       {books.map((book) => (
         <Book
-          key={book.item_id}
-          id={book.item_id}
+          key={book.itemId}
+          id={book.itemId}
           title={book.title}
           author={book.author}
 
